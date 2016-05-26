19cbab0c7f96fce2863c7379004cea69d3c3ff3a
Walter Sobral <wsa@cin.ufpe.br>
2016-05-26 12:31:51 +0000

    Added class-based views to edit and delete Base items.

--- searchie/search_base/urls.py
+++ searchie/search_base/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 from haystack.views import SearchView
-from .views import BaseListView, BaseDetailView
+from .views import BaseListView, BaseDetailView,\
+    BaseUpdateView, BaseCreateView, BaseDeleteView
 
 urlpatterns = [
     url(r'^$', BaseListView.as_view(), name='base-list'),
     url(r'^search/', SearchView(), name='haystack_search'),
     url(r'^(?P<pk>[0-9]+)/$', BaseDetailView.as_view(), name='base-detail'),
+
+    url(r'^(?P<pk>[0-9]+)/create/$', BaseCreateView.as_view(), name='base-create'),
+    url(r'^(?P<pk>[0-9]+)/update/$', BaseUpdateView.as_view(), name='base-update'),
+    url(r'^(?P<pk>[0-9]+)/delete/$', BaseDeleteView.as_view(), name='base-delete'),
 ]
--- searchie/search_base/views.py
+++ searchie/search_base/views.py
@@ -1,8 +1,10 @@
 from datetime import timezone, datetime
 
+from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView,\
+    CreateView, UpdateView, DeleteView
 
 from .models import Base
 
@@ -22,8 +24,22 @@ class BaseDetailView(DetailView):
     model = Base
 
     def get_context_data(self, **kwargs):
-        print(Base.pk)
-        print('\n\n\n\n')
         context = super(BaseDetailView, self).get_context_data(**kwargs)
         context['now'] = datetime.now()
         return context
+
+
+class BaseCreateView(CreateView):
+    model = Base
+    fields = ['value','text']
+
+
+class BaseUpdateView(UpdateView):
+    model = Base
+    fields = ['value','text']
+    template_name_suffix = '_update_form'
+
+
+class BaseDeleteView(DeleteView):
+    model = Base
+    success_url = reverse_lazy('base-list')
