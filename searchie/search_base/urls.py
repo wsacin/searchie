a5a7bb4f5aa5ba09571210a182bd5c936b1456aa
Walter Sobral <wsa@cin.ufpe.br>
2016-05-26 23:32:39 +0000

    Fix in Base Doc create url.

--- searchie/search_base/urls.py
+++ searchie/search_base/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^search/', SearchView(), name='haystack_search'),
     url(r'^(?P<pk>[0-9]+)/$', BaseDetailView.as_view(), name='base-detail'),
 
-    url(r'^(?P<pk>[0-9]+)/create/$', BaseCreateView.as_view(), name='base-create'),
+    url(r'^create/$', BaseCreateView.as_view(), name='base-create'),
     url(r'^(?P<pk>[0-9]+)/update/$', BaseUpdateView.as_view(), name='base-update'),
     url(r'^(?P<pk>[0-9]+)/delete/$', BaseDeleteView.as_view(), name='base-delete'),
 ]
